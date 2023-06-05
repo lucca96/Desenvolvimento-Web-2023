623648409aa59ede6e40119349c2d3e8993bacbf
lucca96 <88782132+lucca96@users.noreply.github.com>
2023-06-05 10:51:20 -0300

    Add files via upload

---
+++ aula_29_05/js/maisum.js
@@ -0,0 +1,19 @@
+const body = document.getElementsByTagName('body')[0];
+
+const div = document.createElement('div');
+const imagem = document.createElement('img');
+const legenda = document.createElement('h5');
+
+imagem.src = localStorage.getItem('caminho');
+imagem.width = '100';
+legenda.innerHTML = localStorage.getItem('descricao');
+
+div.appendChild(imagem);
+div.appendChild(legenda);
+body.appendChild(div);
+
+//exercícios:
+/*
+1 - fazer dados persistirem entre sessões
+2 - exibir a imagem bonita e com legenda 
+*/
